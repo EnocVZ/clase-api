6f20308ba050ddd1e12eded329a63b7731b4bc2c
arianm29 <wukonguwur@gmail.com>
2024-04-09 11:19:59 -0600

    Nuevo controller para persona y vehiculo para poder realizar la relacion de los id de estos (aun no funciona, no se como hacerlo)

--- ClaseMiPrimerAPI/Controllers/PersonaVehiculoController.cs
+++ ClaseMiPrimerAPI/Controllers/PersonaVehiculoController.cs
@@ -1,6 +1,32 @@
-﻿namespace ClaseMiPrimerAPI.Controllers
+﻿using ClaseMiPrimerAPI.DbListContext;
+using ClaseMiPrimerAPI.Model;
+using ClaseMiPrimerAPI.view;
+using Microsoft.AspNetCore.Mvc;
+//using Microsoft.EntityFrameworkCore;
+
+
+namespace ClaseMiPrimerAPI.Controllers
 {
-    public class PersonaVehiculoController
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PersonaVehiculoController: ControllerBase
     {
+        private readonly ILogger<PersonaVehiculoController> logger;
+        // private readonly PersonaContext context;
+        private readonly PersonaVehiculoController context;
+        public PersonaVehiculoController(ILogger<PersonaVehiculoController> paramLogger, PersonaVehiculoContext personaVehiculoContext)
+        {
+            logger = paramLogger;
+            //context = personaContext;
+           // context = personaVehiculoContext;
+
+        }
+
+
+
+
+
+
+
     }
 }
