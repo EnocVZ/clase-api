632f0f897bf994ddab90b7104f6492a547100344
danielJC2007 <157769544+danielJC2007@users.noreply.github.com>
2024-04-09 13:54:47 -0600

    CambiosDaniel1

---
+++ ClaseMiPrimerAPI/Controllers/PersonaVehiculoController.cs
@@ -0,0 +1,45 @@
+﻿using ClaseMiPrimerAPI.DbListContext;
+using ClaseMiPrimerAPI.Controllers;
+using ClaseMiPrimerAPI.DbListContext;
+using ClaseMiPrimerAPI.view;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq.Expressions;
+
+namespace ClaseMiPrimerAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PersonaVehiculoController : Controller
+    {
+        private readonly ILogger<PersonaController> logger;
+        private readonly DBcontext context;
+        public PersonaVehiculoController(ILogger<PersonaController> paramLogger, PersonaContext personaContext)
+        {
+            this.logger = paramLogger;
+            context = personaContext;
+        }
+        [HttpDelete]
+        [Route("eliminarPersonaDB")]
+        public async Task<ActionResult<ResponseGetPersona>> eliminarPersonaBD(int id)
+        {
+            try
+            {
+                ResponseGetPersona response = new ResponseGetPersona();
+                var personaEnDB = await context.Persona.FindAsync(id);
+                context.Remove(personaEnDB);
+                await context.SaveChangesAsync();
+
+
+                response.code = 200;
+                response.message = "se guardo";
+                response.error = false;
+                return Ok(response);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex);
+            }
+
+        }
+    }
+}
---
+++ ClaseMiPrimerAPI/Model/PersonaVehiculo.cs
@@ -0,0 +1,9 @@
+﻿namespace ClaseMiPrimerAPI.Model
+{
+    public class PersonaVehiculo
+    {
+        public int Id { get; set; }
+        public string Persona { get; set; }
+        public string Vehiculo { get; set; }
+    }
+}
